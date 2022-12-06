661ac293784b09f06e2c03e1e6ca1ad9acabfa3e
William Giammona <wgiammona@gmail.com>
2022-12-06 18:46:51 +0200

    imported the contract address and abi

--- components/RaffleEntrance.tsx
+++ components/RaffleEntrance.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+import { useWeb3Contract, useMoralis } from "react-moralis";
+import { contractAddresses, abi } from "../constants";
 
 function RaffleEntrance() {
+    type contractAddressesInterface = {
+        [key: string]: string[];
+    };
+
+    const addresses: contractAddressesInterface = contractAddresses;
+    const { chainId: chainIdHex } = useMoralis(); //hex version
+    const chainId = parseInt(chainIdHex!, 16); //decimal version
+    const raffleAddress = chainId in addresses ? addresses[chainId][0] : null;
+
+    // const { data, error, runContractFunction: enterRaffle, isFetching, isLoading } =
+    // useWeb3Contract({
+    //   abi: usdcEthPoolAbi,
+    //   contractAddress: usdcEthPoolAddress,
+    //   functionName: "enterRaffle",
+    //   params: {},
+    //   msg.Value:
+    // });
     return <div>RaffleEntrance</div>;
 }
 
