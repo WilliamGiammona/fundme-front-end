97483e562ee76287be7e75a6bfd13be8d99a8ab7
William Giammona <wgiammona@gmail.com>
2022-12-07 16:43:47 +0200

    several adjustments

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,11 @@ import { ConnectButton } from "web3uikit";
 
 function Header() {
     return (
-        <div>
-            Decentralized Raffle
-            <ConnectButton moralisAuth={false} />
+        <div className="p-5 border-b-4 flex flex-row">
+            <h1 className="py-4 px-4 text-3xl font-blog">Decentralized Raffle</h1>
+            <div className="ml-auto py-2 py-4">
+                <ConnectButton moralisAuth={false} />
+            </div>
         </div>
     );
 }
--- components/RaffleEntrance.tsx
+++ components/RaffleEntrance.tsx
@@ -18,55 +18,54 @@ function RaffleEntrance() {
     const [winner, setWinner] = useState("0x0000000000000000000000000000000000000000");
 
     const dispatch = useNotification();
+    const { data, error, runContractFunction, isFetching, isLoading } = useWeb3Contract({});
 
-    const {
-        data,
-        error,
-        runContractFunction: enterRaffle,
-        isFetching,
-        isLoading,
-    } = useWeb3Contract({
+    const enterRaffleParams = {
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
         params: {},
         msgValue: entranceFee,
-    });
+    };
 
-    const { runContractFunction: getMinEntryFee } = useWeb3Contract({
+    const getMinEntryFeeParams = {
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "getMinEntryFee",
         params: {},
-    });
+    };
 
-    const { runContractFunction: getNumParticipants } = useWeb3Contract({
+    const getNumParticipantsParams = {
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "getNumParticipants",
         params: {},
-    });
+    };
 
-    const { runContractFunction: getWinner } = useWeb3Contract({
+    const getWinnerParams = {
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "getWinner",
         params: {},
-    });
+    };
 
-    const { runContractFunction: requestRandomWords } = useWeb3Contract({
+    const requestRandomWordsParams = {
         abi: abi,
         contractAddress: raffleAddress,
         functionName: "requestRandomWords",
         params: {},
-    });
+    };
 
     async function updateUI() {
-        const entranceFeeFromContract = ((await getMinEntryFee()) as BigNumber).toString();
+        const entranceFeeFromContract = (
+            (await runContractFunction({ params: getMinEntryFeeParams })) as BigNumber
+        ).toString();
         setEntranceFee(entranceFeeFromContract);
-        const numberOfParticipants = ((await getNumParticipants()) as BigNumber).toString();
+        const numberOfParticipants = (
+            (await runContractFunction({ params: getNumParticipantsParams })) as BigNumber
+        ).toString();
         setNumParticipants(numberOfParticipants);
-        const latestWinner = ((await getWinner()) as BigNumber).toString();
+        const latestWinner = ((await runContractFunction({ params: getWinnerParams })) as BigNumber).toString();
         setWinner(latestWinner);
     }
 
@@ -92,42 +91,90 @@ function RaffleEntrance() {
         });
     };
 
+    const handleError = (error: Error) => {
+        dispatch({
+            type: "info",
+            title: "Transaction Notification",
+            message: `Transaction Failed, ${error.message}`,
+            icon: <Bell />,
+            position: "topR",
+        });
+    };
+
     return (
-        <div>
-            {raffleAddress ? (
-                <div>
-                    <button
-                        onClick={async () => {
-                            await enterRaffle({
-                                onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
-                                onError: (error) => console.log(error),
-                            });
-                        }}
-                    >
-                        Enter Raffle
-                    </button>
-                    <br />
-                    RaffleEntranceFee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH
-                    <br />
-                    Number of Participants: {numParticipants}
-                    <br />
-                    Winner: {winner}
-                    <br />
-                    <button
-                        onClick={async () => {
-                            await requestRandomWords({
-                                onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
-                                onError: (error) => console.log(error),
-                            });
-                        }}
-                    >
-                        Start Raffle
-                    </button>
-                </div>
-            ) : (
-                <div>No Raffle Address Detected</div>
-            )}
-        </div>
+        <main>
+            <p className="italic font-serif py-5 border-b-2 ">
+                This raffle is a smart contract on the Goerli test network at address
+                0x45b65cec5d2d590cb90276a04b59a9f0726a1b48. The raffle is programmed to select a random winner from a
+                list of particpants who must pay the posted ticket price. Once the raffle finishes (only the owner can
+                do this), a random number from the chainlink decentralized oracle network is generated and then used to
+                randomly select a winner, who automatically receives all ETh in the contract.
+            </p>
+            <div className="p-5">
+                {raffleAddress ? (
+                    <div>
+                        <div className="border-b-2">
+                            <div className="py-2">
+                                Ticket Price: {ethers.utils.formatUnits(entranceFee, "ether")} ETH
+                            </div>
+                            <div className="py-2">Number of Participants: {numParticipants}</div>
+                            <div className="py-2 font-bold">
+                                Winner: <div className="font-normal">{winner}</div>
+                            </div>
+                        </div>
+                        <div className="pt-5">
+                            <div className="py-5">
+                                <button
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                                    onClick={async () => {
+                                        await runContractFunction({
+                                            params: enterRaffleParams,
+                                            onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
+                                            onError: (error) => {
+                                                handleError(error);
+                                                console.log(error);
+                                            },
+                                        });
+                                    }}
+                                    disabled={isFetching || isLoading}
+                                >
+                                    {isFetching || isLoading ? (
+                                        <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                                    ) : (
+                                        <div>Enter Raffle</div>
+                                    )}
+                                </button>
+                            </div>
+
+                            <div className="py-2">
+                                <button
+                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                                    onClick={async () => {
+                                        await runContractFunction({
+                                            params: requestRandomWordsParams,
+                                            onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
+                                            onError: (error) => {
+                                                handleError(error);
+                                                console.log(error);
+                                            },
+                                        });
+                                    }}
+                                >
+                                    {isFetching || isLoading ? (
+                                        <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                                    ) : (
+                                        <div>Finish Raffle</div>
+                                    )}
+                                </button>
+                                <div className="text-xs">(Only the contract owner can start this raffle)</div>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div>No Raffle Address Detected</div>
+                )}
+            </div>
+        </main>
     );
 }
 
