6e57909dfcfe730dc6b99058dfd24055e75c0e7e
William Giammona <wgiammona@gmail.com>
2022-12-12 15:55:02 +0200

    adjustments for boiler

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,9 @@ import { ConnectButton } from "web3uikit";
 
 function Header() {
     return (
-        <div className="p-5 border-b-4 flex flex-row">
-            <h1 className="py-4 px-4 text-3xl font-blog">Decentralized Raffle</h1>
-            <div className="ml-auto py-2 py-4">
+        <div className="flex flex-row p-5 border-b-4">
+            <h1 className="px-4 py-4 text-3xl font-blog">Decentralized Raffle</h1>
+            <div className="py-2 py-4 ml-auto">
                 <ConnectButton moralisAuth={false} />
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,18 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
-import RaffleEntrance from "../components/RaffleEntrance";
+import Contract from "../components/Contract";
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
                 <title>Raffle</title>
-                <meta
-                    name="description"
-                    content="Decentrlized Smart Contract Raffle"
-                />
+                <meta name="description" content="Decentrlized Smart Contract Raffle" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-            <RaffleEntrance />
+            <Contract />
         </div>
     );
 }
