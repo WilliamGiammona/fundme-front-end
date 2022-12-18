d1baa32a4d0e9a603cbb16c25e0b469a29fe3770
William Giammona <wgiammona@gmail.com>
2022-12-18 14:46:56 +0200

    updated code to work for useWeb3Contract

--- components/FundMe.tsx
+++ components/FundMe.tsx
@@ -12,100 +12,204 @@ function FundMe() {
     const addresses: contractAddressesInterface = contractAddresses;
     const { chainId: chainIdHex, isWeb3Enabled } = useMoralis(); //hex version
     const chainId = parseInt(chainIdHex!, 16); //decimal version
-    const contractAddress = chainId in addresses ? addresses[chainId][0] : "";
+    const FundMeAddress = chainId in addresses ? addresses[chainId][0] : "";
+    const [entranceFee, setEntranceFee] = useState("");
+    const [funderIndex, setFunderIndex]: any = useState("");
+    const [latestFunder, setLatestFunder] = useState("");
+    const [funderAddress, setFunderAddress]: any = useState("");
+    const [lastestFundedAmount, setLastestFundedAmount] = useState("");
 
     const dispatch = useNotification();
     const { error, runContractFunction, isFetching, isLoading } = useWeb3Contract({});
 
-    // const contractFunctionParams = {
-    //     abi: abi,
-    //     contractAddress: raffleAddress,
-    //     functionName: "enterRaffle",
-    //     params: {},
-    //     msgValue: entranceFee,
-    // };
+    const getMinFundAmtParams = {
+        abi: abi,
+        contractAddress: FundMeAddress,
+        functionName: "getMinFundAmt",
+        params: {},
+    };
 
-    // async function updateUI() {
-    //     const entranceFeeFromContract = (
-    //         (await runContractFunction({ params: contractFunctionParams })) as BigNumber
-    //     ).toString();
-    // }
+    const fundParams = {
+        abi: abi,
+        contractAddress: FundMeAddress,
+        functionName: "fund",
+        params: {},
+        msgValue: 500,
+    };
 
-    // useEffect(() => {
-    //     if (isWeb3Enabled) {
-    //         updateUI();
-    //     }
-    // }, [isWeb3Enabled]);
+    const getFundersParams = {
+        abi: abi,
+        contractAddress: FundMeAddress,
+        functionName: "getFunders",
+        params: {
+            index: funderIndex,
+        },
+    };
 
-    // const handleSuccess = async (tx: ContractTransaction) => {
-    //     await tx.wait();
-    //     handleNewNotification(tx);
-    //     updateUI();
-    // };
+    const getAddressToAmountFundedParams = {
+        abi: abi,
+        contractAddress: FundMeAddress,
+        functionName: "getAddressToAmountFunded",
+        params: {
+            funder: funderAddress,
+        },
+    };
 
-    // const handleNewNotification = (tx: ContractTransaction) => {
-    //     dispatch({
-    //         type: "info",
-    //         title: "Transaction Notification",
-    //         message: `Transaction Complete`,
-    //         icon: <Bell />,
-    //         position: "topR",
-    //     });
-    // };
+    async function updateUI() {
+        const entranceFeeFromContract = (await runContractFunction({ params: getMinFundAmtParams })) as BigNumber;
+        const entranceFeeToString = ethers.utils.formatEther(entranceFeeFromContract);
+        setEntranceFee(entranceFeeToString.toString());
+    }
 
-    // const handleError = (error: Error) => {
-    //     dispatch({
-    //         type: "info",
-    //         title: "Transaction Notification",
-    //         message: `Transaction Failed, ${error.message}`,
-    //         icon: <Bell />,
-    //         position: "topR",
-    //     });
-    //     updateUI();
-    // };
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI();
+        }
+    }, [isWeb3Enabled]);
+
+    const handleFund = async () => {
+        await runContractFunction({
+            params: fundParams,
+            onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
+            onError: (error) => {
+                handleError(error);
+                console.log(error);
+            },
+        });
+    };
+
+    const handleFunderIndexChange = async (event: any) => {
+        setFunderIndex(event.target.value);
+    };
+
+    const handleFunderIndexSubmit = async (event: any) => {
+        event.preventDefault();
+        const Funder = (await runContractFunction({ params: getFundersParams })) as BigNumber;
+        const FunderToString = await Funder.toString();
+        setLatestFunder(FunderToString);
+        setFunderIndex("");
+    };
+
+    const handleFunderAddressChange = async (event: any) => {
+        setFunderAddress(event.target.value);
+    };
+
+    const handleFunderAddressSubmit = async (event: any) => {
+        event.preventDefault();
+        const FunderAddress = (await runContractFunction({ params: getAddressToAmountFundedParams })) as BigNumber;
+        const FunderAddressToString = await FunderAddress.toString();
+        console.log(FunderAddressToString);
+        setLastestFundedAmount(FunderAddressToString);
+
+        setFunderAddress("");
+    };
+
+    const handleSuccess = async (tx: ContractTransaction) => {
+        await tx.wait();
+        handleNewNotification(tx);
+        updateUI();
+    };
+
+    const handleNewNotification = (tx: ContractTransaction) => {
+        dispatch({
+            type: "info",
+            title: "Transaction Notification",
+            message: `Transaction Complete`,
+            icon: <Bell />,
+            position: "topR",
+        });
+    };
+
+    const handleError = (error: Error) => {
+        dispatch({
+            type: "info",
+            title: "Transaction Notification",
+            message: `Transaction Failed, ${error.message}`,
+            icon: <Bell />,
+            position: "topR",
+        });
+        updateUI();
+    };
 
     return (
-        <div>Hi</div>
-        // <main>
-        //     <p className="py-5 font-serif italic border-b-2 ">Random Text</p>
-        //     <div className="p-5">
-        //         {FundMeAddress ? (
-        //             <div>
-        //                 {error && <div className="text-red-500">{error.message}</div>}
-        //                 <div className="pt-5">
-        //                     <div className="py-5">
-        //                         <button
-        //                             className="px-4 py-2 ml-auto font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-        //                             onClick={async () => {
-        //                                 // await runContractFunction({
-        //                                 //     params: enterRaffleParams,
-        //                                 //     onSuccess: (tx) => handleSuccess(tx as ContractTransaction),
-        //                                 //     onError: (error) => {
-        //                                 //         handleError(error);
-        //                                 //         console.log(error);
-        //                                 //     },
-        //                                 // });
-        //                             }}
-        //                             disabled={isFetching || isLoading}
-        //                         >
-        //                             {isFetching || isLoading || isWaiting ? (
-        //                                 <div className="w-8 h-8 border-b-2 rounded-full animate-spin spinner-border"></div>
-        //                             ) : (
-        //                                 <div>Function</div>
-        //                             )}
-        //                         </button>
-        //                         <div className="text-xs">
-        //                             (You may need to refresh after your MetaMask transaction goes through, this may take
-        //                             several miniutes)
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         ) : (
-        //             <div>Pleae Connect Your MetaMask Wallet to the Goerli Test Network</div>
-        //         )}
-        //     </div>
-        // </main>
+        <main>
+            <p className="py-5 font-serif italic border-b-2 ">
+                On the Goerli test network at address: 0xb54B26c0c0641339563aCC75910c6052F57F2B07
+            </p>
+            <div className="p-5">
+                {FundMeAddress ? (
+                    <div>
+                        {error && <div className="text-red-500">{error.message}</div>} You are Connected!
+                        <div>
+                            <button
+                                className="px-4 py-2 ml-auto font-bold text-white bg-red-500 rounded hover:bg-red-700"
+                                onClick={handleFund}
+                            >
+                                Fund
+                            </button>
+                        </div>
+                        <div className="py -5"> Minimum Funding Amount: {entranceFee} ETH</div>
+                        <div>
+                            <form onSubmit={handleFunderIndexSubmit}>
+                                <label htmlFor="funders">Get Funders: </label>
+                                <input
+                                    className="px-5 border-2 border-black rounded-lg"
+                                    type="number"
+                                    id="funders"
+                                    name="funders"
+                                    placeholder="Enter index position"
+                                    value={funderIndex}
+                                    onChange={(event) => {
+                                        handleFunderIndexChange(event);
+                                    }}
+                                />
+                                <div>
+                                    <button
+                                        className="px-4 py-2 ml-auto font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                                        type="submit"
+                                    >
+                                        Submit position
+                                    </button>
+                                </div>
+                            </form>
+                            <form onSubmit={handleFunderAddressSubmit}>
+                                <label htmlFor="amount-funded">Get Amount Funded: </label>
+                                <input
+                                    className="px-5 border-2 border-black rounded-lg"
+                                    type="text"
+                                    id="amount-funded"
+                                    name="amount-funded"
+                                    placeholder="Enter funder address"
+                                    value={funderAddress}
+                                    onChange={(event) => {
+                                        handleFunderAddressChange(event);
+                                    }}
+                                />
+                                <div>
+                                    <button
+                                        className="px-4 py-2 ml-auto font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                                        type="submit"
+                                    >
+                                        Submit Funder Address
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div>
+                            <button className="px-4 py-2 ml-auto font-bold text-white bg-red-500 rounded hover:bg-red-700">
+                                Withdraw
+                            </button>
+                        </div>
+                        <div>Latest Funder Address: {latestFunder}</div>
+                        <div>Latest Funder Amount: {lastestFundedAmount} </div>
+                    </div>
+                ) : (
+                    <div>
+                        Pleae Connect Your MetaMask Wallet to the Goerli Test Network {FundMeAddress} {chainId} -
+                    </div>
+                )}
+            </div>
+        </main>
     );
 }
 
