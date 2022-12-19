71b0b561d1522defa96aabd3d961f67c3a572a6d
William Giammona <wgiammona@gmail.com>
2022-12-19 10:08:58 +0200

    transferred code

--- components/EntranceFee.tsx
+++ components/EntranceFee.tsx
@@ -1,34 +1,41 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useWeb3Contract, useMoralis } from "react-moralis";
+import { contractAddresses, abi } from "../constants";
+import { BigNumber, ethers, ContractTransaction } from "ethers";
 
 function EntranceFee() {
-    // const getMinFundAmtParams = {
-    //     abi: abi,
-    //     contractAddress: FundMeAddress,
-    //     functionName: "getMinFundAmt",
-    //     params: {},
-    // };
+    type contractAddressesInterface = {
+        [key: string]: string[];
+    };
 
-    // const fundParams = {
-    //     abi: abi,
-    //     contractAddress: FundMeAddress,
-    //     functionName: "fund",
-    //     params: {},
-    //     msgValue: 500,
-    // };
+    const addresses: contractAddressesInterface = contractAddresses;
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis(); //hex version
+    const chainId = parseInt(chainIdHex!, 16); //decimal version
+    const FundMeAddress = chainId in addresses ? addresses[chainId][0] : "";
+    const [entranceFee, setEntranceFee] = useState("");
 
-    // async function updateUI() {
-    //     const entranceFeeFromContract = (await runContractFunction({ params: getMinFundAmtParams })) as BigNumber;
-    //     const entranceFeeToString = ethers.utils.formatEther(entranceFeeFromContract);
-    //     setEntranceFee(entranceFeeToString.toString());
-    // }
+    const { error, runContractFunction, isFetching, isLoading } = useWeb3Contract({});
 
-    // useEffect(() => {
-    //     if (isWeb3Enabled) {
-    //         updateUI();
-    //     }
-    // }, [isWeb3Enabled]);
+    const getMinFundAmtParams = {
+        abi: abi,
+        contractAddress: FundMeAddress,
+        functionName: "getMinFundAmt",
+        params: {},
+    };
 
-    return <div>EntranceFee</div>;
+    async function updateUI() {
+        const entranceFeeFromContract = (await runContractFunction({ params: getMinFundAmtParams })) as BigNumber;
+        const entranceFeeToString = ethers.utils.formatEther(entranceFeeFromContract);
+        setEntranceFee(entranceFeeToString.toString());
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI();
+        }
+    }, [isWeb3Enabled]);
+
+    return <div>EntranceFee: {entranceFee} ETH</div>;
 }
 
 export default EntranceFee;
