a0ccf7655e64b4d17c3005ac882b434aab8bee64
William Giammona <wgiammona@gmail.com>
2022-12-19 10:01:23 +0200

    initial commit

---
+++ components/EntranceFee.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function EntranceFee() {
+    // const getMinFundAmtParams = {
+    //     abi: abi,
+    //     contractAddress: FundMeAddress,
+    //     functionName: "getMinFundAmt",
+    //     params: {},
+    // };
+
+    // const fundParams = {
+    //     abi: abi,
+    //     contractAddress: FundMeAddress,
+    //     functionName: "fund",
+    //     params: {},
+    //     msgValue: 500,
+    // };
+
+    // async function updateUI() {
+    //     const entranceFeeFromContract = (await runContractFunction({ params: getMinFundAmtParams })) as BigNumber;
+    //     const entranceFeeToString = ethers.utils.formatEther(entranceFeeFromContract);
+    //     setEntranceFee(entranceFeeToString.toString());
+    // }
+
+    // useEffect(() => {
+    //     if (isWeb3Enabled) {
+    //         updateUI();
+    //     }
+    // }, [isWeb3Enabled]);
+
+    return <div>EntranceFee</div>;
+}
+
+export default EntranceFee;
